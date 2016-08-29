7b12a9de6c06f810c49355e1601ca461b2b5558c
= <=>
2016-08-29 10:48:02 +0300

    chore: init logger

--- src/fdbAdapter/FDBAdapter.hx
+++ src/fdbAdapter/FDBAdapter.hx
@@ -1,9 +1,28 @@
 package fdbAdapter;
+import protocol.debug.Types;
+import adapter.DebugSession;
 
 class FDBAdapter extends adapter.DebugSession
 {
     public function new()
     {
+        trace( "created" );
         super();
+    }
+
+    override function initializeRequest(response:InitializeResponse, args:InitializeRequestArguments):Void
+    {
+        
+        sendEvent(new InitializedEvent());
+		// This debug adapter implements the configurationDoneRequest.
+		response.body.supportsConfigurationDoneRequest = true;
+
+		// make VS Code to use 'evaluate' when hovering over source
+		response.body.supportsEvaluateForHovers = true;
+
+		// make VS Code to show a 'step back' button
+		response.body.supportsStepBack = true;
+        trace( 'got initialize request: $response');
+        sendResponse( response );
     }
 }
--- src/fdbAdapter/Main.hx
+++ src/fdbAdapter/Main.hx
@@ -1,8 +1,26 @@
 package fdbAdapter;
 import adapter.DebugSession;
+import js.node.Fs;
 class Main
 {
     public static function main() {
         DebugSession.run( FDBAdapter );
+        setupTrace();
+    }
+
+    static function setupTrace() {
+        haxe.Log.trace = function(v, ?i) {
+            var r = [Std.string(v)];
+            if (i != null && i.customParams != null) {
+                for (v in i.customParams)
+                    r.push(Std.string(v));
+            }
+            Log({type: "INFO", message: r.join(" ")});
+        }
+    }
+
+    static function Log( input:{type:String, message:String} ):Void
+    {
+        Fs.writeFile('log.txt', haxe.Json.stringify(input), 'utf8', function(e){ });
     }
 }
